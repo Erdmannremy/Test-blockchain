47a3b4e30233d94e793c434c3341bb0a6109c191
erdmannremy@gmail.com <erdmannremy@gmail.com>
2024-01-26 09:29:51 +0100

    projet/v18

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -10,52 +10,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent implements OnInit {
-  form :FormGroup;
-
-  constructor (private formGroup: FormGroup) {
-   this.form = this.formGroup.group({
-    username: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl('')
-   });
-
-  }
-
-  ngOnInit() {
-    this.form.valueChanges.subscribe((value) => {
-      this.form.valueChanges.valid;
-    });
- }
   
-  isFormValid() {
-    return this.isFormValid.valid;
-  } 
-   usernameValidator(){
-    if(this.username === ''){
-      return {
-        required: true
-      };
-    }
-   }
-   if (!this.email.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}/)) {
-    return {
-      email: true
-    };
+
+  constructor () {
+   
   }
 
-
-   passwordValidator() 
-  if (this.password === '') {
-    return {
-      required: true
-    };
-  }
-
-  if (this.password.length < 6) {
-    return {
-      minlength: 6
-    };
-  }
+  
 }
 
 
